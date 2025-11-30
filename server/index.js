012d84172bdbb6c041aeec072dc80a9a972a895e
Tomas Tornamira <103047119+TTorna@users.noreply.github.com>
2025-11-30 07:55:54 -0300

    fix general

--- server/index.js
+++ server/index.js
@@ -148,7 +148,7 @@ io.on('connection', (socket) => {
 });
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
